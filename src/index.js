4f483427edc6ff05b708756b003af8af5a043bbd
cpatterson23 <ceddypbody@gmail.com>
2022-04-20 11:56:02 -0500

    arrays pt 3

---
+++ src/index.js
@@ -0,0 +1,21 @@
+const myListDiv = document.getElementById("list");
+const myBooleans = [
+    true,
+    false,
+    true,
+    false,
+    false,
+    true,
+
+];
+
+function myName(name) {
+    const h1 = document.createElement("h1");
+    h1.innerText = name;
+    h1.className = "name";
+    myListDiv.appendChild(h1);
+}
+
+const trueFalse = myBooleans.reverse.map(bool => !bool )
+
+trueFalse.forEach( number=> console.log(number));
